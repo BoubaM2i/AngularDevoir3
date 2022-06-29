820b4d34bfaa86d0e729635eadf882112acca26d
BoubaM2i <boubacar.dembele@labom2iformation.fr>
2022-06-29 14:37:56 +0200

    Devoirfinish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormulaireComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/formulaire/formulaire.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulaire',
+  templateUrl: './formulaire.component.html',
+  styleUrls: ['./formulaire.component.scss']
+})
+export class FormulaireComponent implements OnInit {
+
+  users : any[] = []
+
+  user : FormGroup = this.formBuilder.group({
+    nom : ["", [Validators.required,Validators.minLength(2)]],
+    prenom : ["",[Validators.required,Validators.minLength(2)]],
+    email : ["", [Validators.required,Validators.email]],
+    entreprise : ["", [Validators.required,Validators.minLength(2)]],
+    telephone : ["", [Validators.required ,Validators.minLength(10)]],
+
+  })
+
+  submitted : boolean = false;
+
+  constructor( private formBuilder : FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+
+  private addUser() : void {
+    this.users.push(this.user.value)
+    this.user.reset()
+  }
+
+  onSubmit () : void {
+    this.submitted = true;
+    if (this.user.valid) {
+      this.addUser()
+    } else {
+      console.log("Formulaire invalide"); 
+    }
+  }
+
+  get Bon () {
+    return this.user.controls
+  }
+
+}
